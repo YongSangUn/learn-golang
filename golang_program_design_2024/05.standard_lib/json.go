0083fdfa8896411b913ca470e1486df2c5b429db
SangUn.Yong <sangun.yong@gmail.com>
2024-07-01 21:21:14 +0800

    feat: json complate.

--- golang_program_design_2024/05.standard_lib/json.go
+++ golang_program_design_2024/05.standard_lib/json.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"os"
 	"time"
 )
 
@@ -24,11 +25,15 @@ func main() {
 	marshaling()
 	structTagTest()
 	unmarshaling()
+	marshalError()
+	custom()
+	encodeJson()
+	decodeJson()
 }
 
 func jsonM(data interface{}) {
 	if jsonData, err := json.Marshal(data); err != nil {
-		log.Fatalf("marshaling failed: %s", err)
+		log.Printf("marshaling failed: %s", err)
 	} else {
 		fmt.Println(string(jsonData))
 	}
@@ -79,7 +84,7 @@ func structTagTest() {
 
 func jsonUnm(str string, v any) {
 	if err := json.Unmarshal([]byte(str), v); err != nil {
-		log.Fatalf("unmarshaling failed: %s", err)
+		log.Printf("unmarshaling failed: %s", err)
 	} else {
 		fmt.Printf("%#v\n", v)
 	}
@@ -114,4 +119,93 @@ func unmarshaling() {
 	details := result["details"].(map[string]interface{})
 	age := details["age"].(float64) // ! Note: Numbers in interface{} are treated as float64
 	fmt.Println("Age:", age)
+}
+
+type UserError struct {
+	Name string
+	Age  int
+	// Assuming there is a field here that cannot be serialized.
+	// Data chan struct{} // The channel cannot be represented in JSON.
+}
+
+func marshalError() {
+	// marshaling error
+	u1 := UserError{
+		Name: "Alice",
+		Age:  30,
+		// Data: make(chan struct{}),
+	}
+	bytes, err := json.Marshal(u1)
+	if err != nil {
+		log.Printf("marshaling failed: %v", err)
+	}
+
+	// unmarshaling error
+	fmt.Println(string(bytes))
+	// "age" should be an integer, but a string is given here.
+	var data = []byte(`{"name":"Alice","age":"unknown"}`)
+	var u2 UserError
+	err = json.Unmarshal(data, &u2)
+	if err != nil {
+		// log.Printf("unmarshaling failed: %v", err)
+	}
+	fmt.Printf("%+v\n", u2)
+}
+
+// These processes can be customized by implementing the json.Marshaler and json.Unmarshaler interfaces.
+
+type Color struct {
+	Red   uint8
+	Green uint8
+	Blue  uint8
+}
+
+func (c Color) MarshalJSON() ([]byte, error) {
+	hex := fmt.Sprintf("\"#%02x%02x%02x\"", c.Red, c.Green, c.Blue)
+	return []byte(hex), nil
+}
+
+func (c *Color) UnmarshalJSON(data []byte) error {
+	_, err := fmt.Sscanf(string(data), "\"#%02x%02x%02x\"", &c.Red, &c.Green, &c.Blue)
+	return err
+}
+
+func custom() {
+	c := Color{Red: 255, Green: 99, Blue: 71}
+
+	jsonColor, _ := json.Marshal(c)
+	fmt.Println(string(jsonColor))
+
+	var newColor Color
+	json.Unmarshal(jsonColor, &newColor)
+	fmt.Printf("%#v\n", newColor)
+}
+
+// JSON data can be directly written to any object that implements the io.Writer interface,
+// meaning that JSON data can be directly encoded to files, network connections, and more.
+func encodeJson() {
+	users := []UserError{
+		{Name: "Alice", Age: 30},
+		{Name: "Bob", Age: 25},
+	}
+	file, _ := os.Create("users.json")
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	if err := encoder.Encode(users); err != nil {
+		log.Fatalf("encode error: %v", err)
+	}
+
+}
+
+// json.Decoder can read JSON data directly from any object that implements the io.Reader interface, seeking and parsing JSON objects and arrays.
+func decodeJson() {
+	file, _ := os.Open("users.json")
+
+	var u []UserError
+	decoder := json.NewDecoder(file)
+	if err := decoder.Decode(&u); err != nil {
+		log.Fatalf("decode error: %v", err)
+	}
+	fmt.Println(u)
 }
